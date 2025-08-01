549d90a7637e6e3f79e431daa9dde0ca424b2e09
李银超 <2429162154@qq.com>
2025-08-01 11:31:00 +0800

    [feat]引入全局公用样式

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,9 @@ import Arco from '@arco-design/web-vue';
 import '@arco-design/web-vue/dist/arco.css';
 // import '@/components';
 // import '@/components/index.less';
-import YcUi from 'yc-ui-pro';
-import 'yc-ui-pro/es/style.css';
 
 const app = createApp(App);
 app.use(router);
 app.use(ArcoIcon);
 app.use(Arco);
-app.use(YcUi);
 app.mount('#app');
